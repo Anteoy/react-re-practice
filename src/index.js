e265795e310ec658a4801650f24be70708ab0856
Anteoy <anteoy@gmail.com>
2018-08-04 19:14:17 +0800

    react redux basic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ControlPanel from './views/ControlPanel';
 import './index.css';
-// import ClickCounter  from './ClickCounter';
-import ConunterPanel  from './ControlPanel';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<ConunterPanel />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <ControlPanel />,
+    document.getElementById('root')
+);
